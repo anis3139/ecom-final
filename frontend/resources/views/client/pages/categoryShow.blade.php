0837124c51a1a95910e2e70275e8ff62fca94881
MD. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2021-02-23 18:46:45 +0600

    all page complete

--- frontend/resources/views/client/pages/categoryShow.blade.php
+++ frontend/resources/views/client/pages/categoryShow.blade.php
@@ -159,8 +159,8 @@
                                                             </div>
 
                                                             <!-- Cable Configuration -->
-                                                             <form action="{{ route('client.addCart') }}" id="cartForm" method="post">
-                                                                @csrf
+                                                             <form  id="cartForm" method="post">
+                                                               
                                                             <div class="product-color">
                                                                 <span>Mezerment:</span>
                                                                 <div class="meserment-choose mt-5">
@@ -446,6 +446,45 @@ axios.post('{{ route('client.getsingleProductdata') }}', {
 
     });
 }
+
+
+
+
+$('#cartForm').on('submit',function (event) {
+        event.preventDefault();
+        let formData=$(this).serializeArray();
+        let meserment=formData[0]['value'];
+        let color=formData[1]['value'];
+        let quantity=formData[2]['value'];
+        let product_ids=formData[3]['value'];
+            
+        let url="{{route('client.addCart')}}";
+        axios.post(url,{
+            meserment:meserment,
+            color:color,
+            quantity:quantity,
+            product_id:product_ids
+        }).then(function (response) {
+          console.log(response.data);
+           if(response.status==200 && response.data==1){
+            $('#quick-view-modal').modal('hide');
+            toastr.success('Product Add Successfully');
+
+
+          
+
+
+}
+           else{
+               toastr.error('Product not Added ! Try Again');
+           }
+
+        }).catch(function (error) {
+            toastr.error('Product not Added  ! Try Again');
+        })
+
+
+    })
 
 </script>
 @endsection
