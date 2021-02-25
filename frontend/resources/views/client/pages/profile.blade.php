ddd78b4bd761dd2d35c4185b3bdda9603606c8dc
MD. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2021-02-25 17:12:03 +0600

    some change

--- frontend/resources/views/client/pages/profile.blade.php
+++ frontend/resources/views/client/pages/profile.blade.php
@@ -81,3 +81,108 @@
 
     </div>
 @endsection
+
+
+@section('script')
+    
+<script>
+    
+
+
+
+
+    
+
+    getcartData()
+
+function getcartData() {
+
+    axios.get("{{ route('client.cartData') }}")
+        .then(function(response) {
+
+            if (response.status = 200) {
+                var dataJSON = response.data;
+                var cartData = dataJSON.cart;
+
+                var a = Object.keys(cartData).length;
+               
+
+                $("#cart_quantity").html(a);
+                $("#total_cart_price").html(' &#2547; ' + dataJSON.total);
+                
+                var imageViewHtml = "";
+                $.each(cartData, function (i, item) {
+                    
+                    imageViewHtml += '<li>';
+                    imageViewHtml += '<a class="aa-cartbox-img" href="#"><img src=" ' + cartData[i].image +
+                        ' " alt="img"></a>';
+                    imageViewHtml += '<div class="aa-cartbox-info"> <h4><a href="#">' + cartData[i].title +
+                        '</a> </h4> <p>' + cartData[i].quantity + ' x &#2547; ' + cartData[i].unit_price +
+                        '</p>  </div>';
+                    imageViewHtml += '<div class="aa-remove-product"><button class="cartDeleteIcon" data-id=' +
+                        i +
+                        '  style=" display:inline-block" type="submit" class="fa fa-times"><i class="fa fa-remove"></i></button> </div>';
+                    imageViewHtml += '</li>';
+                });
+
+
+                $('#headerCart').html(imageViewHtml);
+
+                    console.log(a);
+
+                    if (a == 0) {
+                            $("#HeaderPreview").css("display", "none");
+                        }else{
+                            $("#HeaderPreview").css("display", "block");
+                    }
+
+
+                //Carts click on delete icon
+                $(".cartDeleteIcon").click(function() {
+                    var id = $(this).data('id');
+                    $('#CartsDeleteId').html(id);
+                    DeleteDataCart(id);
+                })
+            } else {
+                toastr.error('Something Went Wrong');
+            }
+        }).catch(function(error) {
+
+            toastr.error('Something Went Wrong...');
+        });
+}
+
+
+
+
+
+$('#confirmDeleteCart').click(function() {
+    var id = $('#CartDeleteId').html();
+    var id = $(this).data('id');
+    DeleteDataCart(id);
+})
+
+
+//delete Cart function
+function DeleteDataCart(id) {
+
+    axios.post("{{ route('client.cartRemove') }}", {
+            product_id: id
+        })
+        .then(function(response) {
+
+            if (response.status == 200) {
+                toastr.success('Cart Removed Success.');
+                getcartData();
+            } else {
+                toastr.error('Something Went Wrong');
+            }
+        }).catch(function(error) {
+
+            toastr.error('Something Went Wrong......');
+        });
+}
+
+</script>
+
+@endsection
