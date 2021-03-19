12f6658a289d874c2eda113cc8996001e4e6604b
Didarul Islam <71816650+didarul-islam@users.noreply.github.com>
2021-03-19 14:37:03 +0600

    Complete home Category

--- resources/views/client/partials/HomeCategory.blade.php
+++ resources/views/client/partials/HomeCategory.blade.php
@@ -1,65 +1,65 @@
  <!-- Shop Categories
          ============================================= -->
+
+         @if ($promo_categories->count() >0 )
+             
+        
                     <div class="fancy-title title-border title-center mb-4">
                         <h4>Shop popular categories</h4>
                     </div>
 
                     <div class="row shop-categories clearfix">
+                        @foreach ($promo_categories as $category)
+                        @if ($loop->first)
+                       
                         <div class="col-lg-7">
-                            <a href="#"
-                                style="background: url('{{ asset('client') }}/demos/shop/images/categories/2-1.jpg') no-repeat right center; background-size: cover;">
+                            <a href="{{ route('client.category', $category->slug) }}"
+                                style="background: url('{{$category->icon}}') no-repeat right center; background-size: cover;">
                                 <div class="vertical-middle dark center">
                                     <div class="heading-block m-0 border-0">
-                                        <h3 class="nott font-weight-semibold ls0">For Him</h3>
+                                        <h3 class="nott font-weight-semibold ls0"> {{$category->name}}</h3>
                                     </div>
                                 </div>
                             </a>
                         </div>
+                        @endif
+                        @endforeach
+                        @foreach ($promo_categories as $key=> $category)
+                      
+                        @if($key != 0 && $key == 1 )
+                         
                         <div class="col-lg-5">
-                            <a href="#"
-                                style="background: url('{{ asset('client') }}/demos/shop/images/categories/1.jpg') no-repeat center right; background-size: cover;">
+                            <a href="{{ route('client.category', $category->slug) }}"
+                                style="background: url('{{$category->icon}}">
                                 <div class="vertical-middle dark center">
                                     <div class="heading-block m-0 border-0">
-                                        <h3 class="nott font-weight-semibold ls0">For Her</h3>
+                                        <h3 class="nott font-weight-semibold ls0">{{$category->name}}</h3>
                                     </div>
                                 </div>
                             </a>
                         </div>
+                        @endif
+                        @endforeach
 
+                        @foreach ($promo_categories as $key=> $category)
+                      
+                        @if($key != 0 && $key != 1 )
+                      
                         <div class="col-lg-4">
-                            <a href="#"
-                                style="background: url('{{ asset('client') }}/demos/shop/images/categories/4.jpg') no-repeat center center; background-size: cover;">
+                            <a href="{{ route('client.category', $category->slug) }}"
+                                style="background: url('{{$category->icon}}">
                                 <div class="vertical-middle dark center">
                                     <div class="heading-block m-0 border-0">
-                                        <h3 class="nott font-weight-semibold ls0">Popular Products</h3>
+                                        <h3 class="nott font-weight-semibold ls0">{{$category->name}}</h3>
                                         <small class="button bg-white text-dark button-light button-mini">Browse Now</small>
                                     </div>
                                 </div>
                             </a>
                         </div>
-                        <div class="col-lg-4">
-                            <a href="#"
-                                style="background: url('{{ asset('client') }}/demos/shop/images/categories/3.jpg') no-repeat center center; background-size: cover;">
-                                <div class="vertical-middle dark center">
-                                    <div class="heading-block m-0 border-0">
-                                        <h3 class="nott font-weight-semibold ls0">Footwears</h3>
-                                        <small class="button bg-white text-dark button-light button-mini">Shop Now</small>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-lg-4">
-                            <a href="#"
-                                style="background: url('{{ asset('client') }}/demos/shop/images/categories/5.jpg') no-repeat center center; background-size: cover;">
-                                <div class="vertical-middle dark center">
-                                    <div class="heading-block m-0 border-0">
-                                        <h3 class="nott font-weight-semibold ls0">Sportswear</h3>
-                                        <small class="button bg-white text-dark button-light button-mini">Shop Now</small>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
+                        @endif
+                        @endforeach
+                      
                     </div>
-
+ @endif
                     <!-- Featured Carousel
          ============================================= -->
