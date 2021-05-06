dab57500f3e9972c760eee0cd8485a7bd064b8b2
MD. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2021-05-06 16:01:14 +0600

    Single view Modal Change

--- resources/views/client/component/Modal.blade.php
+++ resources/views/client/component/Modal.blade.php
@@ -1,78 +1,87 @@
-@extends('client.layouts.app')
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
 
-@section('content')
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            <div class="single-product shop-quick-view-ajax">
+                <div class="ajax-modal-title text-center w-100">
+                    <h2 id="pdTitle"></h2>
+                </div>
 
-<div class="single-product shop-quick-view-ajax">
-    <div class="ajax-modal-title">
-        <h2 id="pdTitle"></h2>
-    </div>
+                <div class="product modal-padding">
 
-    <div class="product modal-padding">
+                    <div class="row col-mb-50">
+                        <div class="col-md-6">
+                            <div class="product-image">
+                                <div class="fslider" data-pagi="false">
+                                    <div class="flexslider">
+                                        <div class="slider-wrap">
+                                            <div class="slide22"><a href="#"
+                                                    title="Pink Printed Dress - Front View"><img src=""
+                                                        id="modalSingleImage" alt="Pink Printed Dress"></a></div>
 
-        <div class="row col-mb-50">
-            <div class="col-md-6">
-                <div class="product-image">
-                    <div class="fslider" data-pagi="false">
-                        <div class="flexslider">
-                            <div class="slider-wrap">
-                                <div class="slide"><a href="#" title="Pink Printed Dress - Front View"><img
-                                            src="demos/shop/images/items/new/1.jpg" alt="Pink Printed Dress"></a></div>
-                                <div class="slide"><a href="#" title="Pink Printed Dress - Side View"><img
-                                            src="demos/shop/images/items/new/1-1.jpg" alt="Pink Printed Dress"></a>
+                                        </div>
+                                    </div>
                                 </div>
+                                <div class="sale-flash badge badge-danger p-2" id="inStock"></div>
+
                             </div>
                         </div>
-                    </div>
-                    <div class="sale-flash badge badge-danger p-2">Sale!</div>
-                </div>
-            </div>
-            <div class="col-md-6 product-desc">
-                <div class="product-price"><del>$39.99</del> <ins class="font-weight-semibold" id="pdPrice"></ins></div>
-                <div class="product-rating">
-                    <i class="icon-star3"></i>
-                    <i class="icon-star3"></i>
-                    <i class="icon-star3"></i>
-                    <i class="icon-star-half-full"></i>
-                    <i class="icon-star-empty"></i>
-                </div>
-                <div class="clear"></div>
-                <div class="line"></div>
+                        <div class="col-md-6 product-desc">
+                            <div class="product-price"><del id="pdMainPrice"></del> <ins class="font-weight-semibold"
+                                    id="pdPrice"></ins></div>
 
-                <!-- Product Single - Quantity & Cart Button
-                                ============================================= -->
-                <form class="cart mb-0" method="post" enctype='multipart/form-data'>
-                    <div class="quantity clearfix">
-                        <input type="button" value="-" class="minus">
-                        <input type="text" step="1" min="1" name="quantity" value="1" title="Qty" class="qty"
-                            size="4" />
-                        <input type="button" value="+" class="plus">
-                    </div>
-                    <button type="submit" class="add-to-cart button m-0">Add to cart</button>
-                </form><!-- Product Single - Quantity & Cart Button End -->
+                            <div class="clear"></div>
+                            <div class="line"></div>
+                            <form class="cart mb-0" method="post" enctype='multipart/form-data' id="cartForm">
 
-                <div class="clear"></div>
-                <div class="line"></div>
-                <p>Libero velit id eaque ex quae laboriosam nulla optio doloribus! Perspiciatis, libero, neque,
-                    perferendis at nisi optio dolor!</p>
-                <ul class="iconlist">
-                    <li><i class="icon-caret-right"></i> Dynamic Color Options</li>
-                    <li><i class="icon-caret-right"></i> Lots of Size Options</li>
-                    <li><i class="icon-caret-right"></i> 30-Day Return Policy</li>
-                </ul>
-                <div class="card product-meta mb-0">
-                    <div class="card-body">
-                        <span itemprop="productID" class="sku_wrapper">SKU: <span class="sku">8465415</span></span>
-                        <span class="posted_in">Category: <a href="#" rel="tag">Shoes</a>.</span>
-                        <span class="tagged_as">Tags: <a href="#" rel="tag">Barena</a>, <a href="#"
-                                rel="tag">Blazers</a>, <a href="#" rel="tag">Tailoring</a>, <a href="#"
-                                rel="tag">Unconstructed</a>.</span>
+                                <div class="product-color">
+                                    <span>Mezerment:</span>
+                                    <div class="meserment-choose">
+
+                                    </div>
+                                </div>
+                                <div class="clear"></div>
+                                <div class="line"></div>
+                                <div class="product-color">
+                                    <span>Color:</span>
+                                    <div class="color-choose">
+                                    </div>
+                                </div>
+                                <div class="clear"></div>
+                                <div class="line"></div>
+
+
+                                <div class="quantity clearfix">
+                                    <input type="button" value="-" class="minus">
+                                    <input type="text" step="1" min="1" name="quantity" id="quantity" value="1"
+                                        title="Qty" class="qty" size="4" />
+                                    <input type="button" value="+" class="plus">
+                                </div>
+
+                                <input type="hidden" id="product_ids" name="product_ids">
+                                <button type="submit" class="add-to-cart button m-0">Add to cart</button>
+                            </form>
+
+                            <div class="clear"></div>
+
+                            <div class="line"></div>
+                            <p id="pDescription"></p>
+
+                            <div class="card product-meta mb-0">
+                                <div class="card-body">
+                                    <span class="posted_in">Category: <a href="#" rel="tag" id="pdCategory"></a> &nbsp;
+                                        &nbsp; <a href="" id="modalSingleView" class="add-to-cart button m-0">View
+                                            Details</a></span>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
-
     </div>
 </div>
-@endextends
-
-@endsection
