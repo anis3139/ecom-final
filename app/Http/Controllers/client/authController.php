7080000911e351a6ca905a58ba25cd375c801b78
MD. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2021-06-02 03:13:19 +0600

    Login Redirect Complete

--- app/Http/Controllers/client/authController.php
+++ app/Http/Controllers/client/authController.php
@@ -22,10 +22,12 @@ use Laravel\Socialite\Facades\Socialite;
 
 class authController extends Controller
 {
-
+    protected $redirectTo;
     public function __construct()
     {
-        $this->middleware('auth',['only'=>'logOut']);
+        $this->middleware('auth', ['only' => 'logOut']);
+        $this->redirectTo = url()->previous();
+        Session::put('backUrl', url()->previous());
     }
 
 
@@ -45,25 +47,24 @@ class authController extends Controller
         $token = $serviceUser->token ?? '';
         $password = Hash::make(Str::random(32));
 
-        $user=User::firstOrCreate([
-            'email'=> $email
+        $user = User::firstOrCreate([
+            'email' => $email
         ], [
-            'email'=> $email,
-            'password'=> $password,
-            'phone_number'=> $phone_number,
-            'image'=> $image,
-            'name'=> $name,
-            'provider'=> $service,
-            'provider_id'=> $token,
+            'email' => $email,
+            'password' => $password,
+            'phone_number' => $phone_number,
+            'image' => $image,
+            'name' => $name,
+            'provider' => $service,
+            'provider_id' => $token,
+            'email_verified_at' => Carbon::now(),
+            'email_verification_token' => ''
         ]);
 
         Auth::login($user, true);
-    $massage="Welcome ".$name;
+        $massage = "Welcome " . $name;
         session()->flash('success', $massage);
-        return redirect()->route('client.checkout');
-
-
-
+        return redirect()->intended($this->redirectTo);
     }
 
 
@@ -80,15 +81,15 @@ class authController extends Controller
 
     public function onlogin(Request $request)
     {
-        $validator=Validator::make(request()->all(),[
-            'email'=>'required|email',
-            'password'=>'required',
-           ]);
-           if ($validator->fails()) {
+        $validator = Validator::make(request()->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        if ($validator->fails()) {
             return redirect()->back()->withErrors($validator)->withInput();
-          }
+        }
 
-          $credentials =request()->only(['email', 'password']);
+        $credentials = request()->only(['email', 'password']);
 
 
         if (Auth::attempt($credentials)) {
@@ -97,61 +98,58 @@ class authController extends Controller
                 auth()->logout();
                 session()->flash('warning', 'Your Account is not Active. Please Check Your Email');
                 return redirect()->route('client.login');
-
-            }else {
+            } else {
                 $request->session()->regenerate();
                 session()->flash('success', 'Login Success');
-                return redirect()->route('client.checkout');
-            }
 
+                return $this->redirectTo == "https://giftaecologist.com/login" ? redirect()->route('client.checkout') : redirect()->intended($this->redirectTo);
+            }
         }
         session()->flash('error', 'The provided credentials do not match our records.');
         return redirect()->back();
-
     }
 
     public function logOut()
     {
 
-       auth()->logout();
-       session()->flash('success', 'Logout Success');
-        return redirect()->route('client.home');
+        auth()->logout();
+        session()->flash('success', 'Logout Success');
+        return redirect()->intended($this->redirectTo);
     }
 
     public function addUser(Request $request)
     {
 
 
-       $validator=Validator::make(request()->all(),[
-        'name'=>'required',
-        'email'=>'required | email |unique:users,email',
-        'phone_number'=>'required|min:8| max:16|unique:users,phone_number',
-        'password' => 'min:6|required_with:password_confirmation|same:password_confirmation',
-        'password_confirmation' => 'min:6'
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required',
+            'email' => 'required | email |unique:users,email',
+            'phone_number' => 'required|min:8| max:16|unique:users,phone_number',
+            'password' => 'min:6|required_with:password_confirmation|same:password_confirmation',
+            'password_confirmation' => 'min:6'
 
-       ]);
+        ]);
 
-       if ($validator->fails()) {
-          return redirect()->back()->withErrors($validator)->withInput();
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
         }
-                $name = $request->Input('name');
-                $phone_number = $request->Input('phone_number');
-                $email =strtolower($request->Input('email'));
-                $password =bcrypt( $request->Input('password'));
+        $name = $request->Input('name');
+        $phone_number = $request->Input('phone_number');
+        $email = strtolower($request->Input('email'));
+        $password = bcrypt($request->Input('password'));
 
-                $user=new User();
-                $user->name=$name;
-                $user->phone_number=$phone_number;
-                $user->email=$email;
-                $user->password= $password;
-                $user->email_verification_token= uniqid(time().$email, true).Str::random(40);
-                $user->save();
+        $user = new User();
+        $user->name = $name;
+        $user->phone_number = $phone_number;
+        $user->email = $email;
+        $user->password = $password;
+        $user->email_verification_token = uniqid(time() . $email, true) . Str::random(40);
+        $user->save();
 
-                $user->notify(new registerEmail( $user));
+        $user->notify(new registerEmail($user));
 
         session()->flash('success', 'Registration Successfull! Please Check Your Mail For Active your Account');
         return redirect()->route('client.registration');
-
     }
 
 
@@ -162,154 +160,146 @@ class authController extends Controller
             return redirect()->route('client.home');
         }
 
-        $user=User::where('email_verification_token', $token)->firstOrFail();
+        $user = User::where('email_verification_token', $token)->firstOrFail();
 
         if ($user) {
-            $user->email_verified_at=Carbon::now();
-            $user->email_verification_token=null;
+            $user->email_verified_at = Carbon::now();
+            $user->email_verification_token = null;
             $user->save();
             session()->flash('success', 'Your Account Varified Successfully');
             return redirect()->route('client.login');
         }
-
-
     }
 
 
-public function profile()
-{
+    public function profile()
+    {
 
-    $data= [];
-    $data['orders']=Orders::where('user_id', auth()->user()->id)->orderBy('id', 'DESC')->get();
+        $data = [];
+        $data['orders'] = Orders::where('user_id', auth()->user()->id)->orderBy('id', 'DESC')->get();
 
-    return view('client.pages.Profile', $data);
-}
-
-
-
-public function forgot()
-{
-    return view('client.pages.Forgot');
-}
-
-
-public function forgotPassword(Request $request)
-{
-
-    $user=User::where('email', $request->email)->first();
-
-    if($user == null){
-        return redirect()->back()->with('error', 'Email not Exist !');
+        return view('client.pages.Profile', $data);
     }
 
 
-    Mail::to($user->email)->send(new registrationVarificationMail($user));
 
-    return redirect()->back()->with('success', 'Please Check your Mail For new Password !');
-
-}
-
-
-public function recoverPassWord($id = null)
-{
-
-    if ($id == null) {
-        session()->flash('error', 'Wrong varification Token');
-        return redirect()->route('client.home');
+    public function forgot()
+    {
+        return view('client.pages.Forgot');
     }
 
-    $recoveryTocken = $id;
-    $email = (explode('-', $recoveryTocken))[0];
-    $user=User::where('email',$email)->firstOrFail();
-    if($user){
-        return view('client.pages.RecoverPassword')->with([ 'user'=> $user]);
-     }
 
-}
+    public function forgotPassword(Request $request)
+    {
 
+        $user = User::where('email', $request->email)->first();
 
-public function updatePassword(Request $request)
-{
-    $validator=Validator::make(request()->all(),[
-
-        'email'=>'required | email',
-        'password' => 'min:6|required_with:password_confirmation|same:password_confirmation',
-        'password_confirmation' => 'min:6'
-       ]);
-
-       if ($validator->fails()) {
-          return redirect()->back()->withErrors($validator)->withInput();
+        if ($user == null) {
+            return redirect()->back()->with('error', 'Email not Exist !');
         }
 
-        $data=User::where('email',$request->email)->first();
 
-        $data->password= bcrypt( $request->Input('password'));
+        Mail::to($user->email)->send(new registrationVarificationMail($user));
+
+        return redirect()->back()->with('success', 'Please Check your Mail For new Password !');
+    }
+
+
+    public function recoverPassWord($id = null)
+    {
+
+        if ($id == null) {
+            session()->flash('error', 'Wrong varification Token');
+            return redirect()->route('client.home');
+        }
+
+        $recoveryTocken = $id;
+        $email = (explode('-', $recoveryTocken))[0];
+        $user = User::where('email', $email)->firstOrFail();
+        if ($user) {
+            return view('client.pages.RecoverPassword')->with(['user' => $user]);
+        }
+    }
+
+
+    public function updatePassword(Request $request)
+    {
+        $validator = Validator::make(request()->all(), [
+
+            'email' => 'required | email',
+            'password' => 'min:6|required_with:password_confirmation|same:password_confirmation',
+            'password_confirmation' => 'min:6'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $data = User::where('email', $request->email)->first();
+
+        $data->password = bcrypt($request->Input('password'));
         $data->save();
 
         return redirect()->route('client.login')->with('success', 'Password Reset Successfully');
-}
+    }
 
-public function profileEdit($id)
-{
-    $data=[];
-    $data['user']=User::where('id',$id)->first();
+    public function profileEdit($id)
+    {
+        $data = [];
+        $data['user'] = User::where('id', $id)->first();
 
-    return view('client.pages.ProfileEdit', $data);
-}
+        return view('client.pages.ProfileEdit', $data);
+    }
 
 
-public function upadeteProfile(Request $request, $id)
-{
-    $validator=Validator::make(request()->all(),[
-        'name'=>'required',
-        'address'=>'required',
-        'city'=>'required',
-        'postal_code'=>'required',
-        'email'=>'required | email',
-        'phone_number'=>'required|min:8| max:16',
+    public function upadeteProfile(Request $request, $id)
+    {
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'postal_code' => 'required',
+            'email' => 'required | email',
+            'phone_number' => 'required|min:8| max:16',
 
-       ]);
+        ]);
 
-       if ($validator->fails()) {
-          return redirect()->back()->withErrors($validator)->withInput();
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
         }
-                $name = $request->Input('name');
-                $phone_number = $request->Input('phone_number');
-                $address = $request->Input('address');
-                $city = $request->Input('city');
-                $district = $request->Input('district');
-                $postal_code = $request->Input('postal_code');
-                $email =strtolower($request->Input('email'));
+        $name = $request->Input('name');
+        $phone_number = $request->Input('phone_number');
+        $address = $request->Input('address');
+        $city = $request->Input('city');
+        $district = $request->Input('district');
+        $postal_code = $request->Input('postal_code');
+        $email = strtolower($request->Input('email'));
 
 
 
 
 
-                $user=User::findOrFail($id);
-                $user->name=$name;
-                $user->phone_number=$phone_number;
-                $user->email=$email;
+        $user = User::findOrFail($id);
+        $user->name = $name;
+        $user->phone_number = $phone_number;
+        $user->email = $email;
 
-                if($request->hasFile('image')){
-                $photoPath =  $request->file('image')->store('public');
-                $photoName = (explode('/', $photoPath))[1];
-                $host = $_SERVER['HTTP_HOST'];
-                $protocol = $_SERVER['PROTOCOL'] = isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https://' : 'http://';
-                $location = $protocol . $host .  "/public/storage/" . $photoName;
-                $user->image=$location;
-            }
+        if ($request->hasFile('image')) {
+            $photoPath =  $request->file('image')->store('public');
+            $photoName = (explode('/', $photoPath))[1];
+            $host = $_SERVER['HTTP_HOST'];
+            $protocol = $_SERVER['PROTOCOL'] = isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https://' : 'http://';
+            $location = $protocol . $host .  "/public/storage/" . $photoName;
+            $user->image = $location;
+        }
 
-                $user->address=$address;
-                $user->city=$city;
-                $user->district=$district;
-                $user->postal_code=$postal_code;
-                $user->save();
+        $user->address = $address;
+        $user->city = $city;
+        $user->district = $district;
+        $user->postal_code = $postal_code;
+        $user->save();
 
-                session()->flash('success', 'Profile Updated Successfully');
-                return redirect()->route('client.profile');
-
-}
-
-
-
+        session()->flash('success', 'Profile Updated Successfully');
+        return redirect()->route('client.profile');
+    }
 }
--- resources/views/client/pages/Login.blade.php
+++ resources/views/client/pages/Login.blade.php
@@ -1,9 +1,27 @@
-@extends('client.layouts.app') 
-@section('title', 'Login')
-@section('content')
-    <!-- catg header banner section -->
+@extends('client.layouts.app')
 
-    <!-- / catg header banner section -->
+@section('title', 'Registration')
+@section('css')
+
+    <style>
+        .aa-login-form input[type="email"],
+        .aa-login-form input[type="number"] {
+            border: 1px solid #ccc;
+            font-size: 16px;
+            height: 40px;
+            margin-bottom: 15px;
+            padding: 10px;
+            width: 100%;
+        }
+        }
+
+    </style>
+
+@endsection
+@section('content')
+
+
+
     <!-- Cart view section -->
     <section id="aa-myaccount">
         <div class="container">
@@ -11,60 +29,74 @@
                 <div class="col-md-12">
                     <div class="mt-5">
                         <div class="row">
-                            <div class="col-md-8 offset-md-2"> @include('client.component.Message')
+                            <div class="col-md-8 offset-md-2">
+                                @include('client.component.Message')
                                 @include('client.component.ErrorMessage')
                                 <div class="row mb-3">
                                     <div class="col-md-12 text-center">
-                                        <div class="col-12">
-                                            <h3>Login to your Account</h3>
-                                        </div>
-                                        <a href="{{route('client.SSOLogin', 'facebook')}}" class="button button-large btn-block si-colored si-facebook nott font-weight-normal ls0 center m-0"><i class="icon-facebook-sign"></i> Log in with Facebook</a>
-					
-                                        <a href="{{route('client.SSOLogin', 'google')}}" class=" mt-2 button button-large btn-block si-colored si-google nott font-weight-normal ls0 center m-0"><i class="icon-google"></i> Log in with Google</a>
-                                        
-                                    
-                                        <a href="{{route('client.SSOLogin', 'github')}}" class=" mt-2 button button-large btn-block si-colored si-github nott font-weight-normal ls0 center m-0"><i class="icon-github"></i> Log in with Github</a>
-                
+
+                                        <a href="{{ route('client.SSOLogin', 'facebook') }}"
+                                            class="button button-large btn-block si-colored si-facebook nott font-weight-normal ls0 center m-0"><i
+                                                class="icon-facebook-sign"></i> Sign up with Facebook</a>
+
+                                        <a href="{{ route('client.SSOLogin', 'google') }}"
+                                            class=" mt-2 button button-large btn-block si-colored si-google nott font-weight-normal ls0 center m-0"><i
+                                                class="icon-google"></i> Sign up with Google</a>
+
+
+                                        <a href="{{ route('client.SSOLogin', 'github') }}"
+                                            class=" mt-2 button button-large btn-block si-colored si-github nott font-weight-normal ls0 center m-0"><i
+                                                class="icon-github"></i> Sign up with Github</a>
+
                                     </div>
                                 </div>
                                 <div class="aa-myaccount-register">
-                                    <form id="login-form" name="login-form" class="row" action="{{ route('client.onlogin') }}" method="post">
+
+                                    <form id="login-form" name="login-form" class="row"
+                                        action="{{ route('client.onlogin') }}" method="post">
                                         @csrf
-                                       
+                                        <div class="col-12">
+                                            <h3>Login to your Account</h3>
+                                        </div>
 
                                         <div class="col-12 form-group">
                                             <label for="login-form-username">Username:</label>
-                                            <input type="email" id="login-form-username" name="email" value="" class="form-control" />
+                                            <input type="email" id="login-form-username" name="email" value=""
+                                                class="form-control" />
                                         </div>
 
                                         <div class="col-12 form-group">
                                             <label for="login-form-password">Password:</label>
-                                            <input type="password" id="login-form-password" name="password" value="" class="form-control" />
+                                            <input type="password" id="login-form-password" name="password" value=""
+                                                class="form-control" />
                                         </div>
 
                                         <div class="col-12 form-group">
-                                            <button class="text-light p-1 button button-large si-colored si-github nott font-weight-normal ls0 center  px-3 py-2" id="login-form-submit" name="login-form-submit" value="login">Login</button>
-                                            <div class="float-right">
-                                                
-                                                <a href="{{ route('client.registration') }}" class="d-block text-light p-1 button button-large si-colored si-github nott font-weight-normal ls0 center ">Register now! </a>
-                                                <a href="{{ route('client.forgot') }}" class="d-block text-primary p-1">Forgot Password?</a>
+                                            <div class="float-left">
+                                                <a href="{{ route('client.registration') }}"
+                                                    class="text-primary p-1  font-weight-normal ls0 center ">Register
+                                                    now! </a>
+                                                <a href="{{ route('client.forgot') }}"
+                                                    class="d-block text-primary p-1">Forgot Password?</a>
                                             </div>
-
+                                            <div class="float-right">
+                                                <button
+                                                    class="text-light p-1 button button-large si-colored si-github nott font-weight-normal ls0 center py-2 px-5"
+                                                    id="login-form-submit" name="login-form-submit"
+                                                    value="login">Login</button>
+                                            </div>
                                         </div>
-
-
                                     </form>
                                 </div>
-                                <br><br>
-
-
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
-    </section> <!-- / Cart view section -->
+    </section>
+    <!-- / Cart view section -->
 @endsection
 
 
